4b04523d39f33bedf86783747068cb12c6c36dea
lddelchev <lddelchev@gmail.com>
2021-11-29 10:28:46 +0200

    View usage examples locally and add experimental examples (#1)
    Ticket: [Introduce peer review process for the usage examples](https://kanbanzone.io/b/YA2t2Uox/c/696-Introduce-peer-review-process-for-the-usage-examples)
    
    1. Add option to add experimental examples - examples that will be listed on the index page only if `showExperimental` request param is provided.
    2. Improvements to be able to load pages of individual examples locally (see the Readme)
    3. Document the development and peer review process.

---
+++ Readme.md
@@ -0,0 +1,50 @@
+# Usage examples
+
+TransportAPI usage examples show how our endpoints can be used.
+They are intended for a technical audience, to provide inspiration for
+what can be built, and tutorials to get started, with code that can be
+reused in applications.
+The examples are mini web applications, written in HTML and javascript.
+They focus on processing data from the API, rather than style:
+we don't want functional code getting lost in a sea of styling.
+
+## Development
+
+To serve the page content on `localhost` execute:
+```shell
+ruby -run -e httpd . -p 8080
+```
+in the project root directory and open
+`http://localhost:8080/src?localDevelopment=true&branch=<YOUR DEVELOPMENT BRANCH>`
+
+Please note that to see your local files you have to add query parameter
+`localDevelopment=true`, otherwise the template `.hbs` files will be loaded
+from GitHub pages and examples sources from raw.githubusercontent.com
+
+The page of usage examples use [CodeSandbox](https://codesandbox.io/)
+for the embedded source playground.
+It is online and to load it with your local changes reflected you need to
+push your development git branch and add its name as request parameter `branch`.
+
+Example:
+
+`http://localhost:8080/src?example=bus-stop-timetable&localDevelopment=true&branch=<YOUR DEVELOPMENT BRANCH>`
+
+## Reviewing examples
+### By using the local setup
+
+1. Run the usage examples locally (See [Development](#development))
+2. Open `http://localhost:8080/src?localDevelopment=true&branch=<REVIEWED BRANCH>`
+
+### By using experimental examples in production
+
+The usage examples navigation page supports experimental usage examples
+to perform production quality inspection without affecting the user experience.
+The examples that are marked as experimental won't be shown unless an
+additional request parameter `showExperimental=true` is provided.
+
+Example:
+https://developer.transportapi.com/examples?showExperimental=true
+
+To mark example as experimental add `experimental: true` to the listed
+`examples` in `main.js`
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 /* global $ _ Prism Handlebars */
 
-const ROOT_URL = 'https://transportapi.github.io/usage-examples/src/'
+const GITHUB_PAGES_URL = 'https://transportapi.github.io/usage-examples/src/'
+const RAW_GITHUB_CONTENT_URL = 'https://raw.githubusercontent.com/transportapi/usage-examples/master/'
+const LOCAL_DEVELOPMENT_QUERY_PARAM = 'localDevelopment'
+const BRANCH_QUERY_PARAM = 'branch'
+const SHOW_EXPERIMENTAL_QUERY_PARAM = 'showExperimental'
+
+const urlParams = new URLSearchParams(window.location.search)
+const showExperimental =
+  urlParams.has(SHOW_EXPERIMENTAL_QUERY_PARAM) ? JSON.parse(urlParams.get(SHOW_EXPERIMENTAL_QUERY_PARAM)) : false
+const gitBranch = urlParams.has(BRANCH_QUERY_PARAM) ? urlParams.get(BRANCH_QUERY_PARAM) : 'master'
+
+// For local development load the files from a relative path
+const examplesRootUrl = urlParams.has(LOCAL_DEVELOPMENT_QUERY_PARAM) ? '../' : RAW_GITHUB_CONTENT_URL
+const pageTemplatesRootUrl = urlParams.has(LOCAL_DEVELOPMENT_QUERY_PARAM) ? '' : GITHUB_PAGES_URL
 
 const examples = [
   {
@@ -8,77 +21,92 @@ const examples = [
     description: 'Retrieves information about all buses departing from a stop with a given ATCOCode in the near ' +
       'future',
     directory: 'bus-stop-timetable',
+    experimental: false
   },
   {
     title: 'Bus route geometry',
     description: 'Retrieves the geometry of a bus route specified by operator, line and direction and draws it on a ' +
       'map',
     directory: 'bus-route-geometry',
+    experimental: false
   },
   {
     title: 'Bus service description',
     description: 'Show bus service metadata: the operator, linename, directions and destinations',
     directory: 'bus-service-description',
+    experimental: false
   },
   {
     title: 'Bus service search',
     description: 'List bus services matching the search on operator code and line name',
     directory: 'bus-service-search',
+    experimental: false
   },
   {
     title: 'Bus journey timetable',
     description: 'Show the scheduled timing point stops for a specific bus journey',
     directory: 'bus-journey-timetable',
+    experimental: false
   },
   {
     title: 'Buses on a map',
     description: 'Show the buses for a specific service on a map',
     directory: 'buses-on-a-map',
+    experimental: false
   },
   {
     title: 'Bus full timetable',
     description: 'Show the full timetable for a given service on a given date',
     directory: 'bus-full-timetable',
+    experimental: false
   },
   {
     title: 'Journey planner',
     description: 'Shows a journey plan between two points',
     directory: 'journey-planner',
-  },
+    experimental: false
+  }
 ]
 
-const urlParams = new URLSearchParams(window.location.search)
 if (urlParams.has('example')) {
   const directory = urlParams.get('example')
   const properties = _.find(examples, { directory })
   showExample(properties)
 } else {
-  const properties = { examples: examples, showExample: showExample }
+  const examplesToList = showExperimental ? examples : examples.filter(example => !example.experimental)
+  const properties = {
+    examples: examplesToList,
+    additionalUrlQueryParams: exampleViewUrlQueryParameters(),
+    showExample: showExample
+  }
   loadTemplate('navigation.hbs', properties, () => null)
 }
 
 function loadTemplate (fileName, properties, onLoad) {
-  $.get(ROOT_URL + fileName, templateSource => {
+  $.get(pageTemplatesRootUrl + fileName, templateSource => {
     const template = Handlebars.compile(templateSource)
 
-    properties.root_url = ROOT_URL
+    properties.root_url = pageTemplatesRootUrl
     const html = template(properties)
     $('#app').html(html)
     onLoad()
   })
 }
 
+const exampleFile = examplesRootUrl + 'examples/{{directory}}/{{name}}'
 const fileHtml = `
   <h2 class="source-title">{{name}}</h2>
   <pre class="line-numbers"
        data-download-link
-       data-src="https://raw.githubusercontent.com/transportapi/usage-examples/master/examples/{{directory}}/{{name}}"
+       data-src="${exampleFile}"
   ><code id="{{name}}" {{#if language}}class="language-{{language}}{{/if}}"></code></pre>
 `
 Handlebars.registerPartial('file', fileHtml)
 
 function showExample (exampleProperties) {
-  exampleProperties.root_url = ROOT_URL
+  exampleProperties.root_url = pageTemplatesRootUrl
+  exampleProperties.branch = gitBranch
+
   loadTemplate('example.hbs', exampleProperties, () => {
     // This can't simply be put in the CSS file because Prism.js seemingly redraws the element
     $('pre.line-numbers').css('max-height', '45em')
@@ -86,4 +114,20 @@ function showExample (exampleProperties) {
     // doesn't work without this approach
     setTimeout(() => Prism.highlightAll(), 0)
   })
+}
+
+function exampleViewUrlQueryParameters () {
+  const parameters = {};
+  // Parameter from the current view that if present should propagate to the example views
+  [LOCAL_DEVELOPMENT_QUERY_PARAM, BRANCH_QUERY_PARAM]
+    .filter(param => urlParams.has(param))
+    .forEach(param => {
+      parameters[param] = urlParams.get(param)
+    })
+  return toAdditionalQueryParametersString(parameters)
+}
+
+function toAdditionalQueryParametersString (parameters) {
+  const params = `${Object.entries(parameters).map(entry => `${entry[0]}=${entry[1]}`).join('&')}`
+  return params ? `&${params}` : ''
 }
