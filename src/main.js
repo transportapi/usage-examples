ffbaed7ab26004dbb430b23cb13629bc67b0741c
lyudmil <lddelchev@gmail.com>
2022-02-23 19:54:26 +0200

    wip

--- src/main.js
+++ src/main.js
@@ -23,32 +23,32 @@ const products = [
   {
     id: 'tapi-journey-planner',
     label: 'TAPI Journey Planner',
-    logo: 'media/logo-tapi-journey-planner.svg'
+    logo: 'static/logo-tapi-journey-planner.svg'
   },
   {
     id: 'tapi-bus-information',
     label: 'TAPI Bus Information',
-    logo: 'media/logo-tapi-bus-information.svg'
+    logo: 'static/logo-tapi-bus-information.svg'
   },
   {
     id: 'tapi-bus-performance',
     label: 'TAPI Bus Performance',
-    logo: 'media/logo-tapi-bus-performance.svg'
+    logo: 'static/logo-tapi-bus-performance.svg'
   },
   {
     id: 'tapi-bus-fares',
     label: 'TAPI Bus Fares',
-    logo: 'media/tapi-bus-fares.svg'
+    logo: 'static/tapi-bus-fares.svg'
   },
   {
     id: 'tapi-rail-information',
     label: 'TAPI Rail Information',
-    logo: 'media/logo-tapi-rail-information.svg'
+    logo: 'static/logo-tapi-rail-information.svg'
   },
   {
     id: 'tapi-places',
     label: 'TAPI Places',
-    logo: 'media/logo-tapi-places.svg'
+    logo: 'static/logo-tapi-places.svg'
   }
 ]
 
@@ -58,6 +58,10 @@ const examples = [
     description: 'Retrieves information about all buses departing from a stop with a given ATCOCode in the near ' +
       'future',
     directory: 'bus-stop-timetable',
+    endpoint: {
+      path: 'bus/stop/.../timetable',
+      documentationUrl: 'https://developer.transportapi.com/docs?raml=https://transportapi.com/raml/transportapi.raml'
+    },
     experimental: false,
     product_ids: ['tapi-bus-information']
   },
@@ -66,6 +70,10 @@ const examples = [
     description: 'Retrieves the geometry of a bus route specified by operator, line and direction and draws it on a ' +
       'map',
     directory: 'bus-route-geometry',
+    endpoint: {
+      path: 'bus/route/.../timetable',
+      documentationUrl: 'https://developer.transportapi.com/docs?raml=https://transportapi.com/raml/transportapi.raml'
+    },
     experimental: false,
     product_ids: ['tapi-bus-information']
   },
@@ -73,6 +81,10 @@ const examples = [
     title: 'Bus service description',
     description: 'Show bus service metadata: the operator, linename, directions and destinations',
     directory: 'bus-service-description',
+    endpoint: {
+      path: 'bus/services/{service}',
+      documentationUrl: 'https://developer.transportapi.com/docs?raml=https://transportapi.com/raml/transportapi.raml'
+    },
     experimental: false,
     product_ids: ['tapi-bus-information']
   },
@@ -80,6 +92,10 @@ const examples = [
     title: 'Bus service search',
     description: 'List bus services matching the search on operator code and line name',
     directory: 'bus-service-search',
+    endpoint: {
+      path: 'bus/services',
+      documentationUrl: 'https://developer.transportapi.com/docs?raml=https://transportapi.com/raml/transportapi.raml'
+    },
     experimental: false,
     product_ids: ['tapi-bus-information']
   },
@@ -87,6 +103,10 @@ const examples = [
     title: 'Bus journey timetable',
     description: 'Show the scheduled timing point stops for a specific bus journey',
     directory: 'bus-journey-timetable',
+    endpoint: {
+      path: 'bus/route/.../timetable',
+      documentationUrl: 'https://developer.transportapi.com/docs?raml=https://transportapi.com/raml/transportapi.raml'
+    },
     experimental: false,
     product_ids: ['tapi-bus-information']
   },
@@ -94,6 +114,10 @@ const examples = [
     title: 'Buses on a map',
     description: 'Show the buses for a specific service on a map',
     directory: 'buses-on-a-map',
+    endpoint: {
+      path: 'bus/service_timetables',
+      documentationUrl: 'https://developer.transportapi.com/docs?raml=https://transportapi.com/raml/transportapi.raml'
+    },
     experimental: false,
     product_ids: ['tapi-bus-information']
   },
@@ -101,6 +125,10 @@ const examples = [
     title: 'Bus full timetable',
     description: 'Show the full timetable for a given service on a given date',
     directory: 'bus-full-timetable',
+    endpoint: {
+      path: 'bus/service_timetables',
+      documentationUrl: 'https://developer.transportapi.com/docs?raml=https://transportapi.com/raml/transportapi.raml'
+    },
     experimental: false,
     product_ids: ['tapi-bus-information']
   },
@@ -108,6 +136,10 @@ const examples = [
     title: 'Journey planner',
     description: 'Shows a journey plan between two points',
     directory: 'journey-planner',
+    endpoint: {
+      path: 'public/journey/from/{from}/to/{to}',
+      documentationUrl: 'https://developer.transportapi.com/docs?raml=https://transportapi.com/raml/transportapi.raml'
+    },
     experimental: false,
     product_ids: ['tapi-journey-planner']
   },
@@ -115,6 +147,10 @@ const examples = [
     title: 'Train station timetable',
     description: 'Create a departure board for a train station, based on scheduled departure times',
     directory: 'train-station-timetable',
+    endpoint: {
+      path: 'train/station/{trainStation}/timetable',
+      documentationUrl: 'https://developer.transportapi.com/docs?raml=https://transportapi.com/raml/transportapi.raml'
+    },
     experimental: false,
     product_ids: ['tapi-rail-information']
   },
@@ -122,6 +158,10 @@ const examples = [
     title: 'Places text search',
     description: 'Perform a text search on the Places endpoint, and show matching places as a list',
     directory: 'places-text-search',
+    endpoint: {
+      path: 'places.json',
+      documentationUrl: 'https://developer.transportapi.com/docs?raml=https://transportapi.com/raml/transportapi.raml'
+    },
     experimental: false,
     product_ids: ['tapi-places']
   }
@@ -130,6 +170,9 @@ const examples = [
 if (urlParams.has('example')) {
   const directory = urlParams.get('example')
   const properties = _.find(examples, { directory })
+  const exampleProductId = properties.product_ids[0]
+  const product = _.find(products, { id: exampleProductId })
+  properties.product = { label: product.label, logo: product.logo }
   showExample(properties)
 } else {
   const examplesToList = showExperimental ? examples : examples.filter(example => !example.experimental)
